dc3d9cf3dbca0116d4eef617dd6069d37c391413
komaweb <marimoxinzhong@gmail.com>
2026-06-28 15:36:27 +0900

    Update models.js

--- js/models.js
+++ js/models.js
@@ -1,1 +1,66 @@
 
+//======================================
+// Original Gacha Maker
+// models.js
+// データモデル
+//======================================
+
+
+//======================================
+// Character
+//======================================
+
+export function createCharacter() {
+
+    return {
+
+        id: crypto.randomUUID(),
+
+        gachaId: "",
+
+        name: "",
+
+        rarity: 1,
+
+        image: "",
+
+        icon: "",
+
+        quote: "",
+
+        description: ""
+
+    };
+
+}
+
+
+//======================================
+// Gacha
+//======================================
+
+export function createGacha() {
+
+    return {
+
+        id: crypto.randomUUID(),
+
+        name: "",
+
+        banner: "",
+
+        guarantee: 1,
+
+        rates: {
+
+            1: 40,
+            2: 30,
+            3: 18,
+            4: 10,
+            5: 2
+
+        }
+
+    };
+
+}
