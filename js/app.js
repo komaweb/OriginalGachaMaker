fdd2e629a460a3cc81dcecf271ea1e4ee316a98b
komaweb <marimoxinzhong@gmail.com>
2026-06-28 14:38:34 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 
+const buttons = document.querySelectorAll(".tab-button");
+const pages = document.querySelectorAll(".page");
+
+buttons.forEach(button => {
+
+    button.addEventListener("click", () => {
+
+        buttons.forEach(btn => btn.classList.remove("active"));
+        pages.forEach(page => page.classList.remove("active"));
+
+        button.classList.add("active");
+
+        const target = button.dataset.page;
+
+        document
+            .getElementById(target)
+            .classList.add("active");
+
+    });
+
+});
+
+console.log("Original Gacha Maker 起動");
