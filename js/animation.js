71a833d069e2da8ed5a185c96f079ef47a2a8d9a
komaweb <marimoxinzhong@gmail.com>
2026-06-28 16:54:30 +0900

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -1,2 +1,23 @@
+//======================================
+// animation.js
+//======================================
 
-// 今後実装
+export function wait(ms){
+
+    return new Promise(resolve=>{
+
+        setTimeout(resolve,ms);
+
+    });
+
+}
+
+export async function poyon(element){
+
+    element.classList.add("poyon");
+
+    await wait(280);
+
+    element.classList.remove("poyon");
+
+}
