a7084a3efec0eee7a1114f0e1a4c47e6c0debb15
komaweb <marimoxinzhong@gmail.com>
2026-06-28 15:36:40 +0900

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,53 @@
 
+//======================================
+// Original Gacha Maker
+// utils.js
+// 共通処理
+//======================================
+
+
+//======================================
+// File → Base64
+//======================================
+
+export function fileToBase64(file){
+
+    return new Promise((resolve,reject)=>{
+
+        const reader = new FileReader();
+
+        reader.onload = ()=>{
+
+            resolve(reader.result);
+
+        };
+
+        reader.onerror = reject;
+
+        reader.readAsDataURL(file);
+
+    });
+
+}
+
+
+//======================================
+// Stars
+//======================================
+
+export function createStars(rarity){
+
+    return "★".repeat(rarity);
+
+}
+
+
+//======================================
+// UUID
+//======================================
+
+export function createId(){
+
+    return crypto.randomUUID();
+
+}
