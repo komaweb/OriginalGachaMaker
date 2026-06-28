290a4105d4b8cc20ecbf3a355756a7cb78aae468
komaweb <marimoxinzhong@gmail.com>
2026-06-28 16:28:46 +0900

    Update gacha.js

--- js/gacha.js
+++ js/gacha.js
@@ -1,2 +1,93 @@
+//======================================
+// Original Gacha Maker
+// gacha.js
+//======================================
 
-// 今後実装
+import {
+
+    getCharacters
+
+} from "./storage.js";
+
+const button =
+    document.getElementById("gachaButton");
+
+const overlay =
+    document.getElementById("gachaOverlay");
+
+const resultImage =
+    document.getElementById("resultImage");
+
+const resultStars =
+    document.getElementById("resultStars");
+
+const resultName =
+    document.getElementById("resultName");
+
+const resultQuote =
+    document.getElementById("resultQuote");
+
+button.addEventListener(
+
+    "click",
+
+    startGacha
+
+);
+
+overlay.addEventListener(
+
+    "click",
+
+    ()=>{
+
+        overlay.classList.remove("show");
+
+    }
+
+);
+
+function startGacha(){
+
+    const characters =
+        getCharacters();
+
+    if(characters.length===0){
+
+        alert("キャラクターが登録されていません。");
+
+        return;
+
+    }
+
+    const character =
+
+        characters[
+
+            Math.floor(
+
+                Math.random()*characters.length
+
+            )
+
+        ];
+
+    resultImage.src =
+        character.image;
+
+    resultName.textContent =
+        character.name;
+
+    resultQuote.textContent =
+        character.quote;
+
+    resultStars.textContent =
+        "★".repeat(character.rarity);
+
+    overlay.classList.add(
+
+        "show"
+
+    );
+
+}
