b90f4dac131a13c230eb861b359eb6190da53201
komaweb <marimoxinzhong@gmail.com>
2026-06-29 19:09:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 import "./js/models.js";
+import "./js/utils.js";
+import "./js/storage.js";
+import "./js/editor.js";
+import "./js/gacha.js";
+import "./js/collection.js";
+import "./js/animation.js";
+import "./js/app.js";
